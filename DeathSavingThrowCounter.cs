5aff85df694024d421db2d4d412510250f961adc
Dennis Grinar <dennis.grinar.jr@gmail.com>
2022-05-17 03:04:26 -0400

    Add new MakeDST(int) & CheckDST

--- DeathSavingThrowCounter.cs
+++ DeathSavingThrowCounter.cs
@@ -22,11 +22,19 @@ namespace Combat_Tracker_Console
         public String MakeDeathSavingThrow(bool roll)
         {
             if (roll) successes++; else failures++;
+            return CheckDeathSavingThrows();
+        }
 
+        public String MakeDeathSavingThrow(int roll)
+        {
+            if (roll > 10) successes++; else failures++;
+            return CheckDeathSavingThrows();
+        }
+
+        private String CheckDeathSavingThrows()
+        {
             if (successes >= 3) return "Unconscious";
-
             else if (failures >= 3) return "Dead";
-
             else return "Dying";
         }
 
